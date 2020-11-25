b5291d856d7402bcd936bfc3f6ca5d9e21910b17
jonterry9 <jterry82@gwu.edu>
2020-11-25 15:00:19 -0500

    prio_set syscall implementation
    system call implemented to set processes' priority
    returns -1 if priority param invalid or proc specified by pid not in curproc's ancestry
    sets proc specified by pid's priority & returns 0 otherwise

--- syscall.c
+++ syscall.c
@@ -97,6 +97,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_prio_set(void);
 
 static int (*syscalls[])(void) = {
   [SYS_fork] sys_fork,   [SYS_exit] sys_exit,     [SYS_wait] sys_wait,     [SYS_pipe] sys_pipe,
@@ -104,7 +105,7 @@ static int (*syscalls[])(void) = {
   [SYS_chdir] sys_chdir, [SYS_dup] sys_dup,       [SYS_getpid] sys_getpid, [SYS_sbrk] sys_sbrk,
   [SYS_sleep] sys_sleep, [SYS_uptime] sys_uptime, [SYS_open] sys_open,     [SYS_write] sys_write,
   [SYS_mknod] sys_mknod, [SYS_unlink] sys_unlink, [SYS_link] sys_link,     [SYS_mkdir] sys_mkdir,
-  [SYS_close] sys_close,
+  [SYS_close] sys_close, [SYS_prio_set] sys_prio_set,
 };
 
 void
